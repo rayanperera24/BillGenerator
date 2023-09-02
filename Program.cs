4767e9c2c1d7fa885bdcd6d6802f857384a2d8a9
Rayan Perera <rayanp2004@gmail.com>
2023-09-02 11:50:07 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Activity___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int price;
+            Console.WriteLine("Enter the unit price of a product : ");
+            price = Convert.ToInt32(Console.ReadLine());
+            int unit;
+            Console.WriteLine("Enter the Number of Units Bought :");
+            unit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Amount : " + price * unit);
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
